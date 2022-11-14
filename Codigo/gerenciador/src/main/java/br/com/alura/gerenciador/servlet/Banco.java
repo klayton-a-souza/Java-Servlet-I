8094e0e09a07a17fe34b34d88970baf327e13e5e
klayton-a-souza <klayton.al.souza@gmail.com>
2022-11-14 12:56:50 -0300

    Codigo 03: Definindo o nosso modelo

--- Codigo/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ Codigo/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,6 +6,17 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
+	
+	static {
+		Empresa alura = new Empresa();
+		alura.setNome("Alura");
+				
+		Empresa caelum = new Empresa();
+		caelum.setNome("Caelum");
+		
+		listaEmpresas.add(alura);
+		listaEmpresas.add(caelum);
+	}
 
 	public void adiciona(Empresa empresa) {
 		listaEmpresas.add(empresa);
