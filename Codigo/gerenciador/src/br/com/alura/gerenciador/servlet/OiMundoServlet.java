08884a520fa9c4d7f0fd286cb46002416ffdcc2d
klayton-a-souza <klayton.al.souza@gmail.com>
2022-11-21 09:14:11 -0300

    Codigo 05: JSTL e Expression Language

--- Codigo/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ Codigo/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -3,25 +3,25 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
-public class OiMundoServlet extends HttpServlet{
-
+//oi
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet {
+	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
 		PrintWriter out = resp.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("Oi mundo, parabens voce escreveu o primeiro servlet");
+		out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
 		out.println("</body>");
-		out.println("<html>");
+		out.println("</html>");
 		
-		System.out.println("O servlet OiMundoServlet foi chamado");
-		
+		System.out.println("o servlet OiMundoServlet foi chamado");
 	}
 }
